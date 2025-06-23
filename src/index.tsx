e2ca184cdd31c8d853ef8d921dfc21e1162ba07b
Ricolito Cantorias <ricolitoc@surefirelocal.com>
2025-06-23 15:30:32 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import 'antd/dist/reset.css';
+import './app/styles/index.css';
+import './app/styles/vvnavbar.style.css';
+import './app/styles/mainpage.mainsection.css';
+import './app/styles/accomodation.section.style.css'
+import App from 'app';
+import reportWebVitals from 'reportWebVitals';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import AppContextProvider from 'app/context';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render_element: React.ReactElement =
+  (<>
+    {/*<React.StrictMode>*/}
+    <HelmetProvider>
+      <AppContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AppContextProvider>
+    </HelmetProvider> 
+    {/*</React.StrictMode>*/}
+  </>);
+root.render(render_element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
